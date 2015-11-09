6318e2249f43bb8a7d5f223b4db6193cae041080
develone <develone@sbcglobal.net>
2015-11-09 13:49:13 -0700

    video image is based on default 640 x 480 or the values passed -wi & -hi or --width & --height These set the camera.resolution

--- picamera/test_video.py
+++ picamera/test_video.py
@@ -3,14 +3,30 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
- 
+import argparse
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-wi", "--width", type=int, default=640, help="image width")
+ap.add_argument("-hi", "--height", type=int, default=480, help="image height")
+args = vars(ap.parse_args())
+#print args 
+#print type(args)
+x = args.get('width')
+#print x
+y = args.get('height')
+#print y
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (640, 480)
+'''video image is based on default 640 x 480 or the values
+passed -wi & -hi or --width & --height
+These set the camera.resolution 
+'''
+camera.resolution = (x, y)
 camera.framerate = 32
 camera.vflip = True
 camera.hflip = True
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(camera, size=(x, y))
  
 # allow the camera to warmup
 time.sleep(0.1)
